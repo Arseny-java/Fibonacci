7c90d5a8cc03aca8c5bd8a65f7461672e71cfaf7
Arseny Sazonov <gwentsenya@gmail.com>
2021-08-02 13:59:02 +0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+       Server server = new Server();
+       Client client = new Client();
+
+       new Thread(server).start();
+       System.out.println("Сервер запущен...");
+       new Thread(client).start();
+
+    }
+}
